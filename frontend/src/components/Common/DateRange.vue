affa2edd39d4a62093db55b1b446990afa33fedc
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2025-10-12 04:40:20 +0700

    ui: change Batches grid to single column and add DateRange padding

--- frontend/src/components/Common/DateRange.vue
+++ frontend/src/components/Common/DateRange.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex items-center text-ink-gray-7">
+	<div class="flex items-center text-ink-gray-7 pb-3">
 		<Calendar class="h-4 w-4 stroke-1.5 mr-2" />
 		<span>
 			{{ getFormattedDateRange(props.startDate, props.endDate) }}

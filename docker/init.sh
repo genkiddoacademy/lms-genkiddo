d820e44d67022e737d240999b437a6b189143e77
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2025-10-15 03:03:11 +0700

    feat: rollback init and docker compose file

--- docker/init.sh
+++ docker/init.sh
@@ -6,35 +6,35 @@ if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     bench start
 else
     echo "Creating new bench..."
-
-    export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
-
-    bench init --skip-redis-config-generation frappe-bench
-
-    cd frappe-bench
-
-    # Use containers instead of localhost
-    bench set-mariadb-host mariadb
-    bench set-redis-cache-host redis://redis:6379
-    bench set-redis-queue-host redis://redis:6379
-    bench set-redis-socketio-host redis://redis:6379
-
-    # Remove redis, watch from Procfile
-    sed -i '/redis/d' ./Procfile
-    sed -i '/watch/d' ./Procfile
-
-    bench get-app lms
-
-    bench new-site lms.localhost \
-    --force \
-    --mariadb-root-password 123 \
-    --admin-password admin \
-    --no-mariadb-socket
-
-    bench --site lms.localhost install-app lms
-    bench --site lms.localhost set-config developer_mode 1
-    bench --site lms.localhost clear-cache
-    bench use lms.localhost
-
-    bench start
 fi
+
+export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
+
+bench init --skip-redis-config-generation frappe-bench
+
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis://redis:6379
+bench set-redis-queue-host redis://redis:6379
+bench set-redis-socketio-host redis://redis:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app lms
+
+bench new-site lms.localhost \
+--force \
+--mariadb-root-password 123 \
+--admin-password admin \
+--no-mariadb-socket
+
+bench --site lms.localhost install-app lms
+bench --site lms.localhost set-config developer_mode 1
+bench --site lms.localhost clear-cache
+bench use lms.localhost
+
+bench start
